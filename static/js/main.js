4d8072b8b8f642aea7b4efd7e1387d6c07023357
Hex <Hex90@riseup.net>
2022-08-14 13:55:38 -0400

    Return to "Scanning for next party info..." interim page.

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,20 @@
  */
 (function () {
     var el = document.getElementById('sizzle-reel');
+    var vids = JSON.parse(el.dataset.videos);
+    var i = Math.floor(Math.random() * vids.length);
     el.addEventListener('ended', function () {
-        // Just redirect when we're done watching the sizzle reel for this party.
-        window.location =  window.location.protocol + '//' + window.location.host + '/assets/hex90-trailer/trailer.html';
+        i = i + 1;
+        if (! vids[i]) {
+            i = 0;
+        }
+        this.setAttribute('src', vids[i].src);
+        this.setAttribute('poster', vids[i].poster);
+        this.load();
+        this.querySelector('a').setAttribute('href', vids[i].src);
+    });
+    document.addEventListener('DOMContentLoaded', function () {
+        el.setAttribute('src', vids[i].src);
+        el.querySelector('a').setAttribute('href', vids[i].src);
     });
 })();
